9d2048a983964d06554a59853452898d12da42a0
ZhenDos <ZhenDos1303@yandex.ru>
2020-10-07 19:40:45 +0300

    Correct

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,7 +4,8 @@ public class Main {
 
     public static void main(String[] args) {
 	    int a = 100;
-	    int c = a+b;
+        int b = 100;
+	    int c = a + b;
 	    // фвапфвапфвапфвапфвапфвап
     }
 }
