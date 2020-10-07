6557a967c2b3ac7e58c512a82a1070e1b1248038
doctor608 <InFo4U2004@mail.ru>
2020-10-07 19:47:26 +0300

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	src/com/company/Main.java
    yes

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,9 +4,17 @@ public class Main {
 
     public static void main(String[] args) {
 	    int a = 100;
+<<<<<<< Updated upstream
         int b=0;
 	    int c = a+b;
-	    // фвапфвапфвапфвапфвапфвап
+	    // фвапфвапфвапфвапфвапфвyes
         //hi 
+=======
+        //da
+        int b = 100;
+	    int c = a+b;
+	    // фвапфвапфвапфвапфвапфвап
+        //hi
+>>>>>>> Stashed changes
     }
 }
